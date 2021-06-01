b43a91115116fb97b30468433768ed06c0ea97fe
황 지환 <76830553+Ji-Hwan94@users.noreply.github.com>
2021-06-01 16:43:50 +0900

    Add files via upload

---
+++ HelloWorld/src/main/java/org/zerock/config/HomeController.java
@@ -0,0 +1,40 @@
+package org.zerock.config;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+//		spring은 jsp와 다르게 request가 아닌 model로 attribute를 보낸다.
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home"; // home.jsp로 이동 한다. (jsp에서의 sendRedirect와 같다.)
+	}
+	
+}
